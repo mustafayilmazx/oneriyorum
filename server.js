9bae5109e761c4b48bf83d709dbdf2f39233c5be
Mustafa Yılmaz <deneme@denee.com>
2022-11-24 11:34:08 +0300

    Basic Get and Post Methods

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
 
 app.get("/", function(req, res){
     res.send("Hello World")
+});
+
+
+app.get("/:id", function(req, res){
+    const id = req.params.id
+    res.send(`Hello ${id}`)
 })
 
+
+app.post("/create-post", function(req,res){
+    const data = req.body;
+    const title = data.title;
+    const content = data.content;
+    console.log(title + ' ' + content);
+    res.write(`Hello ${title}`);
+    res.status(201).send()
+})
 
 app.listen(3000)
