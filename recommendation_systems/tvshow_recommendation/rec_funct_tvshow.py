7d822ff269988e3dc97e119bfa7c52db94c4c278
ismailucrn <ucuranismail0@gmail.com>
2022-12-19 08:59:56 +0300

    recommendation systems tamamlandi
    bütün öneri sistemleri tamamlandı, istenilen girdiye göre dictionary şeklinde geri dönderme işlemleri tamamlandı. fonksiyonlar front-end'den gelecek bilgiye göre geri dönderilecek

--- recommendation_systems/tvshow_recommendation/rec_funct_tvshow.py
+++ recommendation_systems/tvshow_recommendation/rec_funct_tvshow.py
@@ -38,44 +38,78 @@ def get_recommendation(root):
 
 liste = ["Stranger Things", "Deadly Sins", "Winx Club", "Riverdale", "Lupin"]
 
-#get recommendation for list and append 
-result = []
-for i in range(len(liste)):
-    result.append(get_recommendation(liste[i]))
+def tvshow_recommender(liste):
+    result = []
+    for i in range(len(liste)):
+        result.append(get_recommendation(liste[i]))
+    result = [item for sublist in result for item in sublist]
+    final = []
+    for i in range(len(result)):
+        final.append(get_recommendation(result[i]))
+    final = [item for sublist in final for item in sublist]
+    for i in range(len(liste)):
+        if liste[i] in final:
+            final = list(filter(lambda a: a != liste[i], final))
+    tv_dict = {}
+    for i in range(len(final)):
+        if final[i] in tv_dict:
+            tv_dict[final[i]] += 1
+        else:
+            tv_dict[final[i]] = 1
+    tv_dict = {k: v for k, v in sorted(tv_dict.items(), key=lambda item: item[1], reverse=True)}
+    return (list(tv_dict.keys())[0:5])
 
-#concat every value in list
-result = [item for sublist in result for item in sublist]
+#print(tvshow_recommender(liste))
 
-final = []
-for i in range(len(result)):
-    final.append(get_recommendation(result[i]))
+def tvshow_dict(liste):
+    tvshow_dict_list = []
+    for tvshow in tvshow_recommender(liste):
+        tvshow_dict_list.append(df.loc[df['title'] == tvshow].to_dict())
+    return tvshow_dict_list
 
-#concat every value in final list
-final = [item for sublist in final for item in sublist]
-
-
-
-
-#delete if in liste
-for i in range(len(liste)):
-    if liste[i] in final:
-        final = list(filter(lambda a: a != liste[i], final))
+print(tvshow_dict(liste))
 
 
 
-#create dict to count the number of times a movie appears in the list
-tv_dict = {}
 
-for i in range(len(final)):
-    if final[i] in tv_dict:
-        tv_dict[final[i]] += 1
-    else:
-        tv_dict[final[i]] = 1
 
-#sort the dict by the number of times a movie appears in the list
-tv_dict = {k: v for k, v in sorted(tv_dict.items(), key=lambda item: item[1], reverse=True)}
 
-print(list(tv_dict.keys())[0:5])
+# #get recommendation for list and append 
+# result = []
+# for i in range(len(liste)):
+#     result.append(get_recommendation(liste[i]))
 
-#return row of the dataframe with the movie name
-#print(df.loc[df['title'] == list(tv_dict.keys())[0]])
+# #concat every value in list
+# result = [item for sublist in result for item in sublist]
+
+# final = []
+# for i in range(len(result)):
+#     final.append(get_recommendation(result[i]))
+
+# #concat every value in final list
+# final = [item for sublist in final for item in sublist]
+
+
+# #delete if in liste
+# for i in range(len(liste)):
+#     if liste[i] in final:
+#         final = list(filter(lambda a: a != liste[i], final))
+
+
+# #create dict to count the number of times a movie appears in the list
+# tv_dict = {}
+
+# for i in range(len(final)):
+#     if final[i] in tv_dict:
+#         tv_dict[final[i]] += 1
+#     else:
+#         tv_dict[final[i]] = 1
+
+# #sort the dict by the number of times a movie appears in the list
+# tv_dict = {k: v for k, v in sorted(tv_dict.items(), key=lambda item: item[1], reverse=True)}
+
+# print(list(tv_dict.keys())[0:5])
+
+# #return row of the dataframe with the movie name
+# #print(df.loc[df['title'] == list(tv_dict.keys())[0]])
+
